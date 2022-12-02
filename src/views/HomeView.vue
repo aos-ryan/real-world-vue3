0c4a644e0118aaaab9f5c3758acb3c3a302fa221
ryan <ryan@arrayofstars.com>
2022-12-02 14:25:13 -0500

    initial commit and project setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+        id: 12345,
+        category: 'meeting',
+        title: 'Breakdown with Client',
+        description: 'Meeting with client to breakdown completed project',
+        location: 'virtual',
+        date: 'December 15, 2022',
+        time: '13:00',
+        organizer: 'Ryan Kidd'
+      },
+        {
+        id: 12346,
+        category: 'social',
+        title: 'Lunch with Client',
+        description: 'Lunch with client',
+        location: 'Tobys',
+        date: 'December 18, 2022',
+        time: '13:00',
+        organizer: 'Ryan Kidd'
+      },
+        {
+        id: 12347,
+        category: 'appointment',
+        title: 'Dentist Appointment',
+        description: 'Annual Checkup',
+        location: 'Dr Bastien',
+        date: 'December 25, 2022',
+        time: '16:30',
+        organizer: 'Ryan Kidd'
+      },
+    ]
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div class="events">
+      <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+ .events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+ }
+</style>
